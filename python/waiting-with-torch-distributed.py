02739654df5983a7bc7077f222f7cd66a2067a60
Suha Sabi Hussain <suhashussain1@gmail.com>
2021-12-24 00:26:18 -0500

    Update waiting-with-torch-distributed.py

--- python/waiting-with-torch-distributed.py
+++ python/waiting-with-torch-distributed.py
@@ -6,11 +6,9 @@ def bad():
       req = None
       if rank == 0:
           tensor += 1
-          # Send the tensor to process 1
           req = dist.isend(tensor=tensor, dst=1)
           print('Rank 0 started sending')
       else:
-          # Receive tensor from process 0
           req = dist.irecv(tensor=tensor, src=0)
           print('Rank 1 started receiving')
       req.wait()
