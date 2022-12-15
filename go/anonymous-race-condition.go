349accbaa32f042e3ebae1078f9b52ce86740e8a
GrosQuildu <e2.8a.95@gmail.com>
2022-12-15 14:49:09 +0100

    anonymous-race-condition metadata and more cases

--- go/anonymous-race-condition.go
+++ go/anonymous-race-condition.go
@@ -5,8 +5,23 @@ import (
 	"sync"
 )
 
+type myval struct {
+	s string
+}
+type myval2 struct {
+	s myval
+}
+
+func (v *myval) Method(x string) {
+	fmt.Println(v.s + "-" + x)
+}
+
 var (
 	numbers = [6]string{"one", "two", "three", "four", "five", "six"}
+)
+
+var (
+	values = [3]myval2{myval2{myval{"a"}}, myval2{myval{"b"}}, myval2{myval{"c"}}}
 )
 
 func main() {
@@ -17,6 +32,8 @@ func main() {
 	AnonRaceCond_2()
 	AnonRaceCond_2_FP()
 	AnonRaceCond_3_FP()
+	AnonRaceCond_4()
+	AnonRaceCond_4_FP()
 }
 
 func func1() {
@@ -26,6 +43,7 @@ func func1() {
 func func2() {
 	fmt.Println("I am function func2")
 }
+
 
 func ConcurrentFunctions_TP(fns ...func()) {
 	var wg sync.WaitGroup
@@ -117,8 +135,38 @@ func AnonRaceCond_3_FP() {
 	// ok: anonymous-race-condition
 	for i := 0; i< len(numbers); i++ {
 		cpy := i
+		wg.Add(1)
 		go func() {
 			fmt.Println(cpy)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
+
+func AnonRaceCond_4() {
+	var wg sync.WaitGroup
+	// ruleid: anonymous-race-condition
+	for _, val := range values {
+		wg.Add(1)
+		go func() {
+			val.s.Method("test")
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
+
+func AnonRaceCond_4_FP() {
+	var wg sync.WaitGroup
+	// ok: anonymous-race-condition
+	for _, val := range values {
+		wg.Add(1)
+		val := val
+		go func() {
+			val.s.Method("test")
 			wg.Done()
 		}()
 	}
