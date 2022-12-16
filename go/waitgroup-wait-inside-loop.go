6c9e4f2cb014d5e5ecd77c9b534535a6a09653f5
GrosQuildu <e2.8a.95@gmail.com>
2022-12-16 08:42:06 +0100

    waitgroup - more cases

--- go/waitgroup-wait-inside-loop.go
+++ go/waitgroup-wait-inside-loop.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	test1()
 	test2()
+	test3()
 }
 
 func test1() {
@@ -18,9 +19,9 @@ func test1() {
 	var wg sync.WaitGroup
 	var x int32 = 0
 	for i := 0; i < 100; i++ {
+		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			wg.Add(1)
 			atomic.AddInt32(&x, 1)
 		}()
 		wg.Wait()
@@ -31,13 +32,31 @@ func test1() {
 }
 
 func test2() {
+	// ruleid: waitgroup-wait-inside-loop
+	var wg sync.WaitGroup
+	var x int32 = 0
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			atomic.AddInt32(&x, 1)
+			wg.Done()
+		}()
+		wg.Wait()
+	}
+
+	fmt.Println("Wait ...")
+	fmt.Println(atomic.LoadInt32(&x))
+}
+
+
+func test3() {
 	// ok: waitgroup-wait-inside-loop
 	var wg sync.WaitGroup
 	var x int32 = 0
 	for i := 0; i < 100; i++ {
+		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			wg.Add(1)
 			atomic.AddInt32(&x, 1)
 		}()
 	}
