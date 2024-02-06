64457d379e459ed1e59ca00ffc753849da0ff0d7
Mike Shirley <mshirley@itbit.com>
2024-02-06 10:30:56 -0600

    Update generic/tar-insecure-flags.sh
    Co-authored-by: mschwager <mschwager@users.noreply.github.com>

--- generic/tar-insecure-flags.sh
+++ generic/tar-insecure-flags.sh
@@ -13,6 +13,7 @@ tar -xvf --Psomeotherflag archive.tar
 tar -xvf archive.tar
 
 # Unbounded ellipsis span in pattern wraps to next command in some scenarios
+# ok: tar-insecure-flags
 wget https://git.kernel.org/torvalds/t/linux-6.8-rc1.tar.gz -O - | tar -xz -C / && mv linux-6.8-rc1 linux
 
 # This shouldn't match, but the ellipsis wrap picks up the p flag in this command and associates it with the previous command
