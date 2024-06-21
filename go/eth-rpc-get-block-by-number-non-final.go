a07cc267de68684a11c6050a0b36c67af9fa4f3f
Benjamin Samuels <1222451+bsamuels453@users.noreply.github.com>
2024-06-21 12:16:54 -0700

    add getBlockByNumber finality queries

---
+++ go/eth-rpc-get-block-by-number-non-final.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc/types"
+)
+
+func Main() {
+	rpcURL := "https://mainnet.infura.io/v3/YOUR_INFURA_API"
+
+	// Note these clients are often rolled in a separate function or implemented by a non-geth library, so we can't
+	// really scope in on the package imports.
+	client, err := ethclient.Dial(rpcURL)
+
+	// ruleid: eth-rpc-get-block-by-number-non-final
+	currentBlock, err := client.BlockByNumber(context.Background(), types.LatestBlockNumber)
+
+	// ruleid: eth-rpc-get-block-by-number-non-final
+	currentBlock, err := client.BlockByNumber(context.Background(), types.PendingBlockNumber)
+
+	// ruleid: eth-rpc-get-block-by-number-non-final
+	currentBlock, err := client.BlockByNumber(context.Background(), types.EarliestBlockNumber)
+
+	// ruleid: eth-rpc-get-block-by-number-non-final
+	currentBlock, err := client.BlockByNumber(context.Background(), nil)
+
+	// ok: eth-rpc-get-block-by-number-non-final
+	currentBlock, err := client.BlockByNumber(context.Background(), types.FinalizedBlockNumber)
+
+	// ok: eth-rpc-get-block-by-number-non-final
+	currentBlock, err := client.BlockByNumber(context.Background(), types.SafeBlockNumber)
+
+	// ok: eth-rpc-get-block-by-number-non-final
+	currentBlock, err := client.BlockByNumber(context.Background(), 5000000)
+}
