769020ae056879f7dca3ec8bea25352eaee80afa
Benjamin Samuels <1222451+bsamuels453@users.noreply.github.com>
2024-06-21 12:35:16 -0700

    add snap sync rules

---
+++ go/eth-geth-snap-protocol-name-unmodified.go
@@ -0,0 +1,36 @@
+package snap
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/myforkcompany/go-myfork/common"
+	"github.com/myforkcompany/go-myfork/core/types"
+	"github.com/myforkcompany/go-myfork/rlp"
+)
+
+
+// ruleid: eth-geth-snap-protocol-name-unmodified
+const ProtocolName = "snap"
+
+// hey it works.
+import (
+	"errors"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+
+// Constants to match up protocol versions and messages
+const (
+	SNAP1 = 1
+)
+
+// ProtocolName is the official short name of the `snap` protocol used during
+// devp2p capability negotiation.
+// ok: eth-geth-snap-protocol-name-unmodified
+const ProtocolName = "snap"
+
