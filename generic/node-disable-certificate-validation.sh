1914df5eb2e159b9a4188fbe25f96c1ab0f5367a
GrosQuildu <e2.8a.95@gmail.com>
2024-12-05 11:30:55 +0100

    ok example for node

--- generic/node-disable-certificate-validation.sh
+++ generic/node-disable-certificate-validation.sh
@@ -3,4 +3,10 @@
 # ruleid: node-disable-certificate-validation
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
+# ok: node-disable-certificate-validation
+export NODE_TLS_REJECT_UNAUTHORIZED=1
+
+# ok: node-disable-certificate-validation
+export NODE_TLS_REJECT_UNAUTHORIZED=false
+
 node app.js
