68d5c26bdab9c182564e2381b52af4e905e892ad
GrosQuildu <e2.8a.95@gmail.com>
2024-12-05 11:41:13 +0100

    postgres tls - more precise rule

--- generic/postgres-insecure-sslmode.sh
+++ generic/postgres-insecure-sslmode.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
 
+# default sslmode is only "prefer"
+# ruleid: postgres-insecure-sslmode
+psql postgresql://myapplicationuser:mypass@myhost:1234/applicationdb
+
+# ruleid: postgres-insecure-sslmode
+psql "postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?something=else"
+
 # ruleid: postgres-insecure-sslmode
 psql postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?sslmode=disable
 
+# ruleid: postgres-insecure-sslmode
+psql postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?sslmode=PREfered
+
+# "This option is deprecated in favor of the sslmode setting."
+# ruleid: postgres-insecure-sslmode
+psql postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?requiressl=0
+
+# ruleid: postgres-insecure-sslmode
+psql "postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?ssl=false"
+
 # ok: postgres-insecure-sslmode
 psql postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?sslmode=require
+
+# ok: postgres-insecure-sslmode
+psql "postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?sslmode=verify-full&something=else"
+
+# ok: postgres-insecure-sslmode
+psql "postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?sslmode=require&something=else"
+
+# ok: postgres-insecure-sslmode
+psql 'postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?sslmode=require&something=else'
+
+echo '
+# ok: postgres-insecure-sslmode
+postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?sslmode=require&something=else
+'
+
+echo '
+# ok: postgres-insecure-sslmode
+postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?sslmode=require
+'
+
+# "for compatibility with JDBC connection URIs, instances of ssl=true are translated into sslmode=require."
+# ok: postgres-insecure-sslmode
+psql "postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?ssl=true"
+
+# ok: postgres-insecure-sslmode
+psql "postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?ssl=true&something=else"
+
+# ok: postgres-insecure-sslmode
+psql 'postgresql://myapplicationuser:mypass@myhost:1234/applicationdb?requiressl=1'
