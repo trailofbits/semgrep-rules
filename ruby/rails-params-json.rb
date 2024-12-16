22a8f8acff2649e0938d93e9556e9fd5a32ca83b
Matt Schwager <matt.schwager@trailofbits.com>
2024-12-16 09:27:46 -0500

    Add additional params methods

--- ruby/rails-params-json.rb
+++ ruby/rails-params-json.rb
@@ -13,6 +13,12 @@ class ProductsController < ApplicationController
     id4 = params[:something][:id]
 
     # ruleid: rails-params-json
+    id5 = params.fetch(:_json)
+
+    # ruleid: rails-params-json
+    id6 = params.fetch(:_json, {})
+
+    # ruleid: rails-params-json
     product_params = params.require(:_json).map do |product|
       product.permit(:name, :price)
     end
