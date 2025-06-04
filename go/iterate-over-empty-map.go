b9dddbff19037f3e3855e1faca4eb66571687d94
GrosQuildu <e2.8a.95@gmail.com>
2025-06-04 11:13:05 +0200

    more tests

--- go/iterate-over-empty-map.go
+++ go/iterate-over-empty-map.go
@@ -7,12 +7,36 @@ import (
 )
 
 func main() {
+	emptyMapIter()
+	notEmptyMapIter()
 	iter1()
+	iter2()
+	iter3()
 	iter1_FP_1()
 	iter1_FP_2()
 	iter1_FP_3()
 	iter1_FP_4()
 	iter1_FP_5()
+	iter1_FP_6()
+	iter1_FP_7()
+}
+
+func emptyMapIter() {
+  // ruleid: iterate-over-empty-map
+  c := make(map[string]string)
+  for k := range c {
+    // do stuff
+  }
+}
+
+func notEmptyMapIter() {
+   // ok: iterate-over-empty-map
+  c := map[string]string {
+    "foo": "bar"
+  }
+  for k := range c {
+    // do stuff
+  }
 }
 
 func iter1(){
@@ -23,6 +47,25 @@ func iter1(){
 	for v := range m {
 		fmt.Println("map item: ", v)
 	}
+}
+
+func iter2(){
+	// https://github.com/semgrep/semgrep/issues/9558
+	// todoruleid: iterate-over-empty-map
+	m := make(map[string]int, 10)
+
+	fmt.Println("iterating")
+	for v := range m {
+		fmt.Println("map item: ", v)
+	}
+}
+
+func iter3(){
+       // ruleid: iterate-over-empty-map
+       m := map[string]int{}
+       for v := range m {
+               fmt.Println("map item: ", v)
+       }
 }
 
 func iter1_FP_1(){
@@ -75,8 +118,7 @@ func iter1_FP_4(){
 }
 
 func iter1_FP_5() {
-	// https://github.com/semgrep/semgrep/issues/9558
-	// todook: iterate-over-empty-map
+	// ok: iterate-over-empty-map
 	testMap := map[string]int{
 		"foo": 1,
 		"bar": 2,
@@ -89,21 +131,21 @@ func iter1_FP_5() {
 	}
 }
 
+func iter1_FP_6(){
+       // ok: iterate-over-empty-map
+       m := make(map[string]int, 1)
 
-func emptyMapIter() {
-  // ruleid: iterate-over-empty-map
-  c := make(map[string]string)
-  for k := range c {
-    // do stuff
-  }
+       m["v1"]--
+
+       for v := range m {
+               fmt.Println("map item: ", v)
+       }
 }
 
-func notEmptyMapIter() {
-   // ok: iterate-over-empty-map
-  c := map[string]string {
-    "foo": "bar"
-  }
-  for k := range c {
-    // do stuff
-  }
+func iter1_FP_7(){
+       // ok: iterate-over-empty-map
+       m := map[string]int{"test": 1}
+       for v := range m {
+               fmt.Println("map item: ", v)
+       }
 }
